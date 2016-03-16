109af49766b64d8ca6b56afcacffbec40bdbb2e1
Christian Cueni <christian.cueni@gmail.com>
2016-03-16 09:55:13 +0100

    adds navigation indication

--- app/topmenu.component.ts
+++ app/topmenu.component.ts
@@ -2,7 +2,7 @@
  * Created by christiancueni on 06/03/16.
  */
 import {Component} from 'angular2/core';
-import {RouterLink} from 'angular2/router';
+import {RouterLink, Router} from 'angular2/router';
 
 @Component({
     selector: 'topmenu',
@@ -11,4 +11,15 @@ import {RouterLink} from 'angular2/router';
 })
 export class TopMenuComponent {
 
+    private navClass = 'main';
+
+    constructor(private router:Router) {
+        router.subscribe((route) => {
+            if (route.indexOf('books') === 0) {
+                this.navClass = 'books';
+            } else {
+                this.navClass = 'main';
+            }
+        });
+    }
 }
